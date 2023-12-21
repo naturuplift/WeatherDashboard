a75fa1238b9cb586127aff4276a8604726d711f8
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2023-12-21 12:33:14 -0400

    script comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -38,16 +38,23 @@ $('#searchButton').on('click', function () {
 // function to update seach history, call weather API methods
 // and call functions to display current and 5-day forecast
 function weatherDashboard(City) {
+
   // Call geaolocation to find city lat, lon cordinates
   cityGeocodingAPI(City)
+
     .then(([lat, lon]) => {
+
       console.log(City, lat, lon) // TODO comment when tested
       // Call function to display city current forecast
       currentWeather(City, lat, lon);
       // Call function to display city 5-day forecast
+
     })
+
     .catch(error => {
+
       console.error("Error calling cityGeocodingAPI:", error)
+
     });
 }
 
@@ -55,125 +62,186 @@ function weatherDashboard(City) {
 // show City, Date, Weather-Icon
 // show Temperature, Wind-Speed and Humidity
 function currentWeather(City, lat,lon) {
+
   // Get the current day using Day.js
   let today = dayjs();
   // split address at comma
   const parts = City.split(",");
   // extract city from string
   const extractedString = parts[0];
+
   // display name of location, date and weather icon
   cityDisplayandDate = `${extractedString} (` + today.format('D/MM/YYYY') + ')';
+
+  // set display to html card for current weather
   $('.current-card h5').text(cityDisplayandDate);
+
   // display current temperature, wind speed and humidity
   // display 5-day forecast for location
 }
 
 // find city temperature
 function cityGeocodingAPI(cityGeo) {
+
   // configure recipe API method and parameters
   let AUTH = 'appid=22c3eb9ede3421b2f412a925f97e46ae';
   let limit = 'limit=1';
   const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityGeo + '&' + AUTH + '&' + limit;
+
   // return a fetch promise that resolves with city latitude and longitude
   return new Promise((resolve, reject) => {
+
     fetchData(apiUrl)
+
       .then(data => {
+
         const latCity = data[0].lat;
         const lonCity = data[0].lon;
-        resolve([latCity, lonCity]); //return latitude and longitude for city
+
+        //return latitude and longitude for city
+        resolve([latCity, lonCity]);
+
       })
-      .catch(error => { // catch error
-        reject('Error fetching Geocoding:',error); // reject Promise if catch error
+
+      // catch error
+      .catch(error => {
+
+        // reject Promise if catch error
+        reject('Error fetching Geocoding:',error);
+
     });
   });
 }
 
 // find city latitude and longitude from city,state code,country code
 function currentWeatherForecast(cityGeo) {
+
   // configure recipe API method and parameters
   let AUTH = 'appid=22c3eb9ede3421b2f412a925f97e46ae';
   let limit = 'limit=1';
   const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityGeo + '&' + AUTH + '&' + limit;
+
   // return a fetch promise that resolves with city latitude and longitude
   return new Promise((resolve, reject) => {
+
     fetchData(apiUrl)
+
       .then(data => {
+
         const latCity = data[0].lat;
         const lonCity = data[0].lon;
-        resolve([latCity, lonCity]); //return latitude and longitude for city
+
+        //return latitude and longitude for city
+        resolve([latCity, lonCity]);
+
       })
-      .catch(error => { // catch error
-        reject('Error fetching Geocoding:',error); // reject Promise if catch error
+
+      // catch error
+      .catch(error => {
+
+        // reject Promise if catch error
+        reject('Error fetching Geocoding:',error);
+
     });
   });
 }
 
 // find city latitude and longitude from city,state code,country code
 function fiveDayWeatherForecast(cityGeo) {
+
   // configure recipe API method and parameters
   let AUTH = 'appid=22c3eb9ede3421b2f412a925f97e46ae';
   let limit = 'limit=1';
   const apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityGeo + '&' + AUTH + '&' + limit;
+
   // return a fetch promise that resolves with city latitude and longitude
   return new Promise((resolve, reject) => {
+
     fetchData(apiUrl)
+
       .then(data => {
+
         const latCity = data[0].lat;
         const lonCity = data[0].lon;
-        resolve([latCity, lonCity]); //return latitude and longitude for city
+
+        //return latitude and longitude for city
+        resolve([latCity, lonCity]);
+
       })
-      .catch(error => { // catch error
-        reject('Error fetching Geocoding:',error); // reject Promise if catch error
+
+      // catch error
+      .catch(error => {
+
+        // reject Promise if catch error
+        reject('Error fetching Geocoding:',error);
+
     });
   });
 }
 
 // here we pass the url we want to call from API and await until fetch responds
 async function fetchData(url) {
+
   const fetcher = await fetch(url)
   const data = await fetcher.json();
+
   return data;
 }
 
 // Function to check if a city is already in history
 function isCityInHistory(checkCity) {
+
   // split address at comma
   const parts = checkCity.split(",");
+
   // extract city from string
   const extractedString = parts[0];
   const historyItems = $('#searchHistory li');
+
   for (let i = 0; i < historyItems.length; i++) {
+
     const historyItemButton = historyItems.eq(i).find('.cityButton');
     const historyItemText = historyItemButton.text();
+
     // return true so city is not added to search history
     if (historyItemText === extractedString) {
+
       // City is already in history
       return true;
     }
   }
+
   // City is not in history
   return false;
 }
 
 // show city on search history as a click button
 function updateSearchHistory(updateCity) {
+
   // split address at comma
   const parts = updateCity.split(",");
+
   // extract city from string
   const extractedString = parts[0];
+
   // created button into html and add city searched
   const cityCard = `
     <li class="list-group-item">
       <button class="btn btn-secondary btn-block cityButton" data-location="${updateCity}">${extractedString}</button>
     </li>
   `;
+
   // append city search to search history
   $("#searchHistory").append(cityCard);
+
   // Handle clicks on city buttons
   $("#searchHistory").on("click", ".cityButton", function () {
+
   const cityName = $(this).data("location");
+
   // Handle the click action for the city button
   weatherDashboard(cityName);
+  
   console.log("City selected from search history:", cityName) // TODO comment when tested
   });
 }
